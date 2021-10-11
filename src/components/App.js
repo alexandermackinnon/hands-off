12a0c68f9974b5ce002c4e168c448a52d7de933a
Alexander MacKinnon <alexmackinnon55@gmail.com>
2021-10-11 10:43:15 -0400

    Update typography

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function App() {
-  return <div className="app">HANDS OFF.</div>;
+  return (
+    <div className="app">
+      <h1>HANDS OFF.</h1>
+    </div>
+  );
 }
 
 export default App;
